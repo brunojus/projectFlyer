70d9cd52d8a82a3db8b9d4df1973890af0b4fbca
Bruno Justino <ti@qualconcurso.com.br>
2016-12-11 18:02:20 -0200

    Including gulpfile

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Project Flyer</title>
+	<link rel="stylesheet" type="text/css" href="/css/app.css">
+</head>
+<body>
+
+	<div class="container">
+		@yield('content')
+	</div>
+
+</body>
+</html>
